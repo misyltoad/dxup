5c3ed5711f3732d7790cb7b7f027cd92fc6708c1
Joshua Ashton <joshua@nonagon.games>
2018-12-08 00:22:15 +0000

    [dx9asm, util] Support for other system values (depth, point size, etc.)

--- src/util/shared_conversions.cpp
+++ src/util/shared_conversions.cpp
@@ -1,5 +1,6 @@
 #include "shared_conversions.h"
 #include <array>
+#include <d3dcommon.h>
 
 namespace dxapex {
 
@@ -28,6 +29,17 @@ namespace dxapex {
         return dclUsageMap[14];
 
       return dclUsageMap[usage];
+    }
+
+    uint32_t sysValue(D3DDECLUSAGE usage) {
+      switch (usage) {
+      case D3DDECLUSAGE_POSITION: return D3D_NAME_POSITION;
+      case D3DDECLUSAGE_TESSFACTOR: return D3D_NAME_FINAL_TRI_EDGE_TESSFACTOR;
+      case D3DDECLUSAGE_DEPTH: return D3D_NAME_DEPTH;
+      case D3DDECLUSAGE_SAMPLE: return D3D_NAME_SAMPLE_INDEX;
+      default:
+        return 0;
+      }
     }
 
   }
--- src/util/shared_conversions.h
+++ src/util/shared_conversions.h
@@ -6,6 +6,7 @@ namespace dxapex {
     namespace convert {
 
         const std::string& declUsage(D3DDECLUSAGE usage);
+        uint32_t sysValue(D3DDECLUSAGE usage);
 
     }
 
