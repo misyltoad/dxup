61885e52818fd85207b41f25d4ad6cba4a2f3a22
Joshua Ashton <joshua@nonagon.games>
2019-01-03 01:29:40 +0000

    [util] Add executable name to the log file's name.

--- src/util/log.cpp
+++ src/util/log.cpp
@@ -5,6 +5,9 @@
 #include "windows_includes.h"
 #include "../../version.h"
 
+#include <Shlwapi.h>
+#pragma comment(lib, "Shlwapi.lib")
+
 namespace dxup {
 
   namespace log {
@@ -12,15 +15,27 @@ namespace dxup {
     FILE* logFile = nullptr;
     bool fileValid = false;
 
+    void createLog() {
+      char logName[MAX_PATH];
+      char exePath[MAX_PATH];
+
+      GetModuleFileNameA(NULL, exePath, MAX_PATH);
+      PathRemoveExtensionA(exePath);
+      const char* exeName = PathFindFileNameA(exePath);
+
+      snprintf(logName, MAX_PATH, "%s_d3d9.log", exeName);
+
+      logFile = fopen(logName, "w");
+
+      fileValid = logFile != nullptr;
+
+      if (fileValid)
+        fprintf(logFile, u8"DXUP - 🐸 - Version %s\n", DXUP_VERSION);
+    }
+
     void internal_write(const char* prefix, const char* fmt, va_list list) {
-      if (logFile == nullptr) {
-        logFile = fopen("log_d3d9.txt", "w");
-
-        fileValid = logFile != nullptr;
-
-        if (fileValid)
-          fprintf(logFile, u8"DXUP - 🐸 - Version %s\n", DXUP_VERSION);
-      }
+      if (logFile == nullptr)
+        createLog();
 
       char buffer[1024];
       char buffer2[1024];
