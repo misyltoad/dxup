be24e1982297bf74f36acdee585b2eaa5411843e
Joshua Ashton <joshua@nonagon.games>
2019-01-04 18:39:12 +0000

    [d3d9] Delta constant buffer updates.

--- src/d3d9/d3d9_constant_buffer.cpp
+++ src/d3d9/d3d9_constant_buffer.cpp
@@ -22,8 +22,9 @@ namespace dxup {
     , m_elementCount{ bufferInfo[bufferType].elementCount }
     , m_shaderType{ shaderType }
     , m_bufferType{ bufferType } {
-    m_elements.reset(new uint8_t[m_elementSize * m_elementCount]);
-    std::memset(m_elements.get(), 0, m_elementSize * m_elementCount);
+    std::vector<uint8_t> blankData;
+    blankData.resize(m_elementSize * m_elementCount);
+    std::memset(blankData.data(), 0, m_elementSize * m_elementCount);
 
     D3D11_BUFFER_DESC cbDesc;
     cbDesc.ByteWidth = m_elementSize * m_elementCount;
@@ -34,7 +35,7 @@ namespace dxup {
     cbDesc.StructureByteStride = 0;
 
     D3D11_SUBRESOURCE_DATA data;
-    data.pSysMem = (void*) m_elements.get();
+    data.pSysMem = (void*)blankData.data();
     data.SysMemPitch = m_elementSize * m_elementCount;
     data.SysMemSlicePitch = 0;
 
@@ -42,7 +43,18 @@ namespace dxup {
     if (FAILED(result))
       log::fail("Couldn't create constant buffer.");
 
-    prepareDraw();
+    makeStagingDesc(cbDesc, 0, D3DFMT_UNKNOWN);
+    result = m_device->CreateBuffer(&cbDesc, &data, &m_stagingBuffer);
+    if (FAILED(result))
+      log::fail("Couldn't create staging constant buffer.");
+
+    m_dirtyBox.top = 0;
+    m_dirtyBox.front = 0;
+    m_dirtyBox.bottom = 1;
+    m_dirtyBox.back = 1;
+
+    bind();
+    finishDraw();
   }
 
   void D3D9ConstantBuffer::bind() {
@@ -54,8 +66,14 @@ namespace dxup {
   }
 
   void D3D9ConstantBuffer::prepareDraw() {
+    m_context->Unmap(m_stagingBuffer.ptr(), 0);
     pushData();
-    bind();
+  }
+
+  void D3D9ConstantBuffer::finishDraw() {
+    m_dirtyBox.left = UINT32_MAX;
+    m_dirtyBox.right = 0;
+    m_context->Map(m_stagingBuffer.ptr(), 0, D3D11_MAP_WRITE, 0, &m_stagingMapping);
   }
 
   HRESULT D3D9ConstantBuffer::set(uint32_t index, const void* values, uint32_t count) {
@@ -65,7 +83,11 @@ namespace dxup {
     if (count == 0)
       return D3D_OK;
 
-    std::memcpy(&m_elements.get()[index * m_elementSize], values, m_elementSize * count);
+    m_dirtyBox.left = std::min(index * m_elementSize, m_dirtyBox.left);
+    m_dirtyBox.right = std::max((index + count) * m_elementSize, m_dirtyBox.right);
+
+    uint8_t* data = (uint8_t*)m_stagingMapping.pData;
+    std::memcpy((void*)&data[index * m_elementSize], values, m_elementSize * count);
     return D3D_OK;
   }
 
@@ -76,15 +98,16 @@ namespace dxup {
     if (count == 0)
       return D3D_OK;
 
-    std::memcpy(values, (void**)&m_elements.get()[index * m_elementSize], m_elementSize * count);
+    uint8_t* data = (uint8_t*)m_stagingMapping.pData;
+    std::memcpy(values, (void*)&data[index * m_elementSize], m_elementSize * count);
     return D3D_OK;
   }
 
   void D3D9ConstantBuffer::pushData() {
-    D3D11_MAPPED_SUBRESOURCE res;
-    m_context->Map(m_buffer.ptr(), 0, D3D11_MAP_WRITE_DISCARD, 0, &res);
-    std::memcpy(res.pData, m_elements.get(), m_elementSize * m_elementCount);
-    m_context->Unmap(m_buffer.ptr(), 0);
+    if (m_dirtyBox.left == UINT32_MAX)
+      return;
+
+    m_context->CopySubresourceRegion(m_buffer.ptr(), 0, m_dirtyBox.left, 0, 0, m_stagingBuffer.ptr(), 0, &m_dirtyBox);
   }
 
   //
@@ -100,6 +123,13 @@ namespace dxup {
     for (uint32_t i = 0; i < ShaderType::Count; i++) {
       for (uint32_t j = 0; j < BufferType::Count; j++)
         m_buffers[(j * ShaderType::Count) + i]->prepareDraw();
+    }
+  }
+
+  void D3D9ConstantBuffers::finishDraw() {
+    for (uint32_t i = 0; i < ShaderType::Count; i++) {
+      for (uint32_t j = 0; j < BufferType::Count; j++)
+        m_buffers[(j * ShaderType::Count) + i]->finishDraw();
     }
   }
 
--- src/d3d9/d3d9_constant_buffer.h
+++ src/d3d9/d3d9_constant_buffer.h
@@ -3,7 +3,6 @@
 #include "../dx9asm/dx9asm_meta.h"
 #include "d3d9_base.h"
 #include <array>
-#include <memory>
 
 namespace dxup {
 
@@ -23,6 +22,7 @@ namespace dxup {
     D3D9ConstantBuffer(ID3D11Device1* device, ID3D11DeviceContext1* context, ShaderType shaderType, BufferType bufferType);
 
     void prepareDraw();
+    void finishDraw();
     void bind();
 
     HRESULT set(uint32_t index, const void* values, uint32_t count);
@@ -37,7 +37,6 @@ namespace dxup {
     ID3D11Device1* m_device;
     ID3D11DeviceContext1* m_context;
 
-    std::shared_ptr<uint8_t> m_elements;
     uint32_t m_elementSize;
     uint32_t m_elementCount;
 
@@ -45,7 +44,10 @@ namespace dxup {
     BufferType m_bufferType;
 
     Com<ID3D11Buffer> m_buffer;
+    Com<ID3D11Buffer> m_stagingBuffer;
 
+    D3D11_MAPPED_SUBRESOURCE m_stagingMapping;
+    D3D11_BOX m_dirtyBox;
   };
 
   class D3D9ConstantBuffers {
@@ -58,6 +60,7 @@ namespace dxup {
     HRESULT get(ShaderType shdrType, BufferType bufferType, uint32_t index, void* values, uint32_t count);
 
     void prepareDraw();
+    void finishDraw();
 
   private:
 
